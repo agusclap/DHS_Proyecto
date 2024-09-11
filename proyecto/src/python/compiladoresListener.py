dd93174df8958e8d70a3c7c0c536954a9d8438d5
agusclap <rodeyroagustin@gmail.com>
2024-09-11 10:19:56 -0300

    proyect initialized

---
+++ proyecto/src/python/compiladoresListener.py
@@ -0,0 +1,21 @@
+# Generated from /home/agustin/Desktop/DHS-Proyecto/proyecto/src/python/compiladores.g4 by ANTLR 4.13.1
+from antlr4 import *
+if "." in __name__:
+    from .compiladoresParser import compiladoresParser
+else:
+    from compiladoresParser import compiladoresParser
+
+# This class defines a complete listener for a parse tree produced by compiladoresParser.
+class compiladoresListener(ParseTreeListener):
+
+    # Enter a parse tree produced by compiladoresParser#s.
+    def enterS(self, ctx:compiladoresParser.SContext):
+        pass
+
+    # Exit a parse tree produced by compiladoresParser#s.
+    def exitS(self, ctx:compiladoresParser.SContext):
+        pass
+
+
+
+del compiladoresParser
---
+++ proyecto/src/python/compiladoresVisitor.py
@@ -0,0 +1,18 @@
+# Generated from /home/agustin/Desktop/DHS-Proyecto/proyecto/src/python/compiladores.g4 by ANTLR 4.13.1
+from antlr4 import *
+if "." in __name__:
+    from .compiladoresParser import compiladoresParser
+else:
+    from compiladoresParser import compiladoresParser
+
+# This class defines a complete generic visitor for a parse tree produced by compiladoresParser.
+
+class compiladoresVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by compiladoresParser#s.
+    def visitS(self, ctx:compiladoresParser.SContext):
+        return self.visitChildren(ctx)
+
+
+
+del compiladoresParser
