61a8780bc1b633e24cc2033cbed0a75fba4ad60f
agusclap <rodeyroagustin@gmail.com>
2025-01-24 11:12:01 -0300

    Avanzes en walker.py

--- proyecto/src/python/Walker.py
+++ proyecto/src/python/Walker.py
@@ -2,15 +2,27 @@ from compiladoresVisitor import compiladoresVisitor
 from compiladoresParser import compiladoresParser
 
 class Walker (compiladoresVisitor):
+    _temporales = []
+    _etiquetas = []
+    generador_temporales = Temporales()
+    generador_etiquetas = Etiquetas()
+    isFuncion = 0
+    inFuncion = 0
+
     def visitPrograma(self, ctx: compiladoresParser.ProgramaContext):
-        print("=-"*20)
-        print("--> Comienza a caminar")
-        return super().visitPrograma(ctx)
-    def visitBloque(self, ctx: compiladoresParser.BloqueContext):
-        print("Nuevo Contexto")
-        #return super().visit(ctx)
-        return super().visitInstrucciones(ctx.getChild(1))
-    def visitTerminal(self, node):
-        print("--> Token" + node.getText())
-        return super().visitTerminal(node)
+        print("Generando codigo intermedio".center(40, "*"))
+        self.f = open("./output/CodigoIntermedio.txt", "w")
+
+        self.visitInstrucciones(ctx.getChild(0))
+
+        self.f.close()
+        print("Codigo intermedio generado".center(40, "*"))
+
+    def visitInstrucciones(self, ctx: compiladoresParser.InstruccionContext):
+        self.visitInstruccion(ctx.getChild(0))
+
+        if ctx.getChild(1).getChildCount() != 0:
+            self.visitInstrucciones(ctx.getChild(1))
+
+        return
 
